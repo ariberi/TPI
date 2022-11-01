6ec9f49cf958959eb42f1993b4f87f3c13672b79
Ari Beri <berinsteinariel@gmail.com>
2022-11-01 12:36:07 -0300

    .

---
+++ cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/berin/Documents/facultad/exactas/1ero/ALGO 1/tpi2/lib/googletest-master/googlemock
+# Build directory: C:/Users/berin/Documents/facultad/exactas/1ero/ALGO 1/tpi2/cmake-build-debug/lib/googletest-master/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/berin/Documents/facultad/exactas/1ero/ALGO 1/tpi2/lib/googletest-master/googletest
+# Build directory: C:/Users/berin/Documents/facultad/exactas/1ero/ALGO 1/tpi2/cmake-build-debug/lib/googletest-master/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
