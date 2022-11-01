8cafda8235d755f2493f74ad5d11557c8fd404f4
Ari Beri <berinsteinariel@gmail.com>
2022-11-01 16:23:35 -0300

    Tests agregados

--- tests/ganoTEST.cpp
+++ tests/ganoTEST.cpp
@@ -1,5 +1,78 @@
+// Tests para la función gano.
+// Ver el archovo README.md para ayuda
+
 #include "../ejercicios.h"
 #include "../auxiliares.h"
 #include "gtest/gtest.h"
 
 using namespace std;
+
+// ┌───┬───┬───┬───┬───┐
+// │ * │ * │ 2 │ 1 │ 1 │
+// ├───┼───┼───┼───┼───┤
+// │ 3 │ 3 │ 3 │ * │ 1 │
+// ├───┼───┼───┼───┼───┤
+// │ 1 │ * │ 2 │ 1 │ 1 │
+// ├───┼───┼───┼───┼───┤
+// │ 2 │ 3 │ 2 │ 1 │ 0 │
+// ├───┼───┼───┼───┼───┤
+// │ * │ 2 │ * │ 1 │ 0 │
+// └───┴───┴───┴───┴───┘
+
+tablero t = {
+        { cMINA,  cMINA,  cVACIA, cVACIA, cVACIA },
+        { cVACIA, cVACIA, cVACIA, cMINA,  cVACIA },
+        { cVACIA, cMINA,  cVACIA, cVACIA, cVACIA },
+        { cVACIA, cVACIA, cVACIA, cVACIA, cVACIA },
+        { cMINA,  cVACIA, cMINA,  cVACIA, cVACIA },
+};
+
+vector<pos> posicionesVacias = {
+        {0, 2}, {0, 3}, {0, 4},
+        {1, 0}, {1, 1}, {1, 2}, {1, 4},
+        {2, 0}, {2, 2}, {2, 3}, {2, 4},
+        {3, 0}, {3, 1}, {3, 2}, {3, 3},
+        {4, 1}, {4, 3}
+};
+
+jugadas jugadasTodaviaNoGano = {
+        jugada(pos(0, 2), 2),jugada(pos(0, 4), 1),
+        jugada(pos(1, 0), 3),jugada(pos(1, 1), 3),jugada(pos(1, 2), 3),jugada(pos(1, 4), 1),
+        jugada(pos(2, 0), 1),jugada(pos(2, 2), 2),jugada(pos(2, 4), 1),
+        jugada(pos(4, 1), 2),jugada(pos(4, 3), 1),jugada(pos(4, 4), 0),
+};
+
+jugadas jugadasGanoElJuego = {
+        jugada(pos(0, 2), 2),jugada(pos(0, 3), 1),jugada(pos(0, 4), 1),
+        jugada(pos(1, 0), 3),jugada(pos(1, 1), 3),jugada(pos(1, 2), 3),jugada(pos(1, 4), 1),
+        jugada(pos(2, 0), 1),jugada(pos(2, 2), 2),jugada(pos(2, 3), 1),jugada(pos(2, 4), 1),
+        jugada(pos(3, 0), 2),jugada(pos(3, 1), 3),jugada(pos(3, 2), 2),jugada(pos(3, 3), 1),jugada(pos(3, 4), 0),
+        jugada(pos(4, 1), 2),jugada(pos(4, 3), 1),jugada(pos(4, 4), 0),
+};
+
+jugadas jugadasPerdio = {
+        jugada(pos(0, 2), 2), jugada(pos(0, 4), 1),
+        jugada(pos(2,1), 0)
+};
+
+TEST(ganoTEST, todaviaNoGano){
+    jugadas j = jugadasTodaviaNoGano;
+    ASSERT_FALSE(gano(t, j));
+}
+
+TEST(ganoTEST, ganoElJuego){
+    jugadas j = jugadasGanoElJuego;
+    ASSERT_TRUE(gano(t, j));
+}
+
+TEST(ganoTEST, perdioElJuego){
+    jugadas j = jugadasPerdio;
+    ASSERT_FALSE(gano(t, j));
+
+}
+TEST(ganoTEST, listaVacia){
+    jugadas j = {};
+    ASSERT_FALSE(gano(t, j));
+}
+
+
