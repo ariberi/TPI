c2bc32d79265eac1a26a7c421fbe89946c883c43
Ari Beri <berinsteinariel@gmail.com>
2022-11-02 21:21:11 -0300

    Agregue tests

--- tests/cambiarBanderitaTEST.cpp
+++ tests/cambiarBanderitaTEST.cpp
@@ -1,4 +1,3 @@
-
 #include "../ejercicios.h"
 #include "../auxiliares.h"
 #include "gtest/gtest.h"
@@ -31,3 +30,19 @@ TEST(cambiarBanderitasTest, posSinBanderitaYNoJugada){
     banderitas b_esperada = {make_pair(0,0), make_pair(1,0)};
     ASSERT_EQ(b, b_esperada);
 }
+
+TEST(cambiarBanderitasTest, posSinBanderitaYPreviamenteJugada){
+    tablero tt =
+            {{ cMINA,  cVACIA,  cVACIA },
+             { cMINA, cVACIA, cVACIA},
+             { cVACIA, cVACIA, cVACIA}};
+    jugadas j = {make_pair(make_pair(1,1),1), make_pair(make_pair(2,2),0)};
+    pos p = make_pair(2,2);
+    banderitas b = {make_pair(0,0)};
+    cambiarBanderita(tt, j, p, b);
+    banderitas b_esperada = {make_pair(0,0)};
+    ASSERT_EQ(b, b_esperada);
+}
+
+
+
