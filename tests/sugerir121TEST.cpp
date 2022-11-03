d7c51638b003359f0d6cbedfaf361513d70618b5
Ari Beri <berinsteinariel@gmail.com>
2022-11-02 21:21:45 -0300

    Agregue tests

--- tests/sugerir121TEST.cpp
+++ tests/sugerir121TEST.cpp
@@ -5,4 +5,82 @@
 
 using namespace std;
 
+// ┌───┬───┬───┐
+// │ 1 │ 2 │ 1 │
+// ├───┼───┼───┤
+// │ * │ 2 │ * │
+// ├───┼───┼───┤
+// │ 1 │ 2 │ 1 │
+// └───┴───┴───┘
+
+
+tablero tt_horizontal = {
+        {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+        {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+        {cVACIA, cMINA, cVACIA, cMINA, cVACIA},
+        {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+        {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA}
+};
+
+tablero tt_vertical = {
+        {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+        {cVACIA, cVACIA, cMINA, cVACIA, cVACIA},
+        {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA},
+        {cVACIA, cVACIA, cMINA, cVACIA, cVACIA},
+        {cVACIA, cVACIA, cVACIA, cVACIA, cVACIA}
+};
+
+jugadas jj_horizontal = {
+        jugada(pos(1, 1), 1), jugada(pos(1, 2), 2), jugada(pos(1, 3), 1),
+        jugada(pos(3, 1), 1), jugada(pos(3, 2), 2), jugada(pos(3, 3), 1)
+};
+jugadas jj_vertical = {
+        jugada(pos(1, 1), 1), jugada(pos(1, 3), 1),
+        jugada(pos(2, 1), 2), jugada(pos(2, 3), 2),
+        jugada(pos(3, 1), 1), jugada(pos(3, 3), 1)
+};
+
+
+
+TEST(sugerir121TEST, posicionEnBanderita){
+    pos p = pos(2,2);
+    banderitas b = {pos(2,2)};
+    ASSERT_FALSE(sugerirAutomatico121(tt_horizontal,b,jj_horizontal,p));
+}
+
+TEST(sugerir121TEST, posicionYaJugada){
+    pos p = pos(2,0);
+    jugadas jj1 = {
+            jugada(pos(1, 1), 1), jugada(pos(1, 2), 2), jugada(pos(1, 3), 1),
+            jugada(pos(2,2), 2),
+            jugada(pos(3, 1), 1), jugada(pos(3, 2), 2), jugada(pos(3, 3), 1)};
+    banderitas b = {pos(4,3)};
+    ASSERT_FALSE(sugerirAutomatico121(tt_horizontal,b,jj_horizontal,p));
+}
+
+TEST(sugerir121TEST, noTieneAdyacente121){
+    pos p = pos(0,4);
+    banderitas b = {pos(4,3)};
+    ASSERT_FALSE(sugerirAutomatico121(tt_horizontal,b,jj_horizontal,p));
+}
+
+TEST(sugerir121TEST, posicionConMina){
+    pos p = pos(2,1);
+    banderitas b = {pos(4,3)};
+    ASSERT_FALSE(sugerirAutomatico121(tt_horizontal,b,jj_horizontal,p));
+}
+
+TEST(sugerir121TEST, posicionSugeribleHorizontal){
+    pos p = pos(2,2);
+    banderitas b = {pos(4,3)};
+    ASSERT_TRUE(sugerirAutomatico121(tt_horizontal,b,jj_horizontal,p));
+}
+
+TEST(sugerir121TEST, posicionSugeribleVertical){
+    pos p = pos(2,2);
+    banderitas b = {pos(4,3)};
+    ASSERT_TRUE(sugerirAutomatico121(tt_vertical,b,jj_vertical,p));
+}
+
+
 
